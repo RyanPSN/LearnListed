c57b8c21b13437cb1f26337a1d10b8a5b9ac00c2
RyanPSN <ryangregoire286@gmail.com>
2024-10-03 17:23:25 -0400

    Add project files.

---
+++ LearnListed/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+
+
+        For u As Integer = 0 To 4
+
+            Console.WriteLine("Enter Num: ")
+            Dim num As Integer = Console.ReadLine()
+
+            For j As Integer = num To num + 1
+
+                Console.WriteLine("Enter List: ")
+                Dim List = Console.ReadLine()
+
+                Console.WriteLine("Num: {0} Value: {1}", j, List)
+                Console.ReadLine()
+
+
+
+            Next
+
+        Next
+
+    End Sub
+
+
+
+End Module
---
+++ LearnListed/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LearnListed")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("LearnListed")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6fa443a3-6906-45f1-b327-fb12889a0b4d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
